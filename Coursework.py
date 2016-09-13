d8918516d36cc5a3943f7f84942991bdf6d19654
Jamesray30 <jim.ray19@yahoo.co.uk>
2016-09-13 20:24:51 +0100

    Add files via upload

---
+++ Coursework.py
@@ -0,0 +1,11 @@
+Enteryourusername = "Johanna"
+
+if Enteryourusername is "James":
+    print("welcome to game")
+elif Enteryourusername is "Espen":
+    print("Welcome to game")
+elif Enteryourusername is "tomas":
+    print("Welcome to game")
+else:
+    print("Please register for the game")
+
